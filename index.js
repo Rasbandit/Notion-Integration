0ac4c6412bbed18a6c9be8d0a21368abb18f38ec
Todd <todd.rasband@podium.com>
2021-05-26 09:32:03 -0600

    sleep data runs every hour

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const CronJob = require('cron').CronJob
 const {createNewDay, setSleepData, createNewWeek} = require('./notion');
 
 new CronJob('* * 1 * * *', createNewDay, null, true, process.env.TIME_ZONE);
-new CronJob('* * 12 * * *', setSleepData, null, true, process.env.TIME_ZONE);
+new CronJob('* * */1 * * *', setSleepData, null, true, process.env.TIME_ZONE);
 new CronJob('* * 1 * * 6', createNewWeek, null, true, process.env.TIME_ZONE);
