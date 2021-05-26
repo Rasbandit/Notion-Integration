c2cd0137439106cdbe97e9824f165f4fe5a9a421
Todd <todd.rasband@podium.com>
2021-05-25 19:02:35 -0600

    removed line

--- index.js
+++ index.js
@@ -6,5 +6,3 @@ const {createNewDay, setSleepData} = require('./notion');
 
 new CronJob('* * 1 * * *', createNewDay, null, true, process.env.TIME_ZONE);
 new CronJob('* * 12 * * *', setSleepData, null, true, process.env.TIME_ZONE);
-
-setSleepData()
