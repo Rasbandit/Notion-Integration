2dfa12d80021b65e265fc2b18900bbbc9f9ecb0c
Todd <todd.rasband@podium.com>
2021-05-26 09:35:09 -0600

    fix timelines

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const CronJob = require('cron').CronJob
 
 const {createNewDay, setSleepData, createNewWeek} = require('./notion');
 
-new CronJob('* * 1 * * *', createNewDay, null, true, process.env.TIME_ZONE);
-new CronJob('* * */1 * * *', setSleepData, null, true, process.env.TIME_ZONE);
-new CronJob('* * 1 * * 6', createNewWeek, null, true, process.env.TIME_ZONE);
+new CronJob('0 0 1 * * *', createNewDay, null, true, process.env.TIME_ZONE);
+new CronJob('0 0 */1 * * *', setSleepData, null, true, process.env.TIME_ZONE);
+new CronJob('0 0 1 * * 6', createNewWeek, null, true, process.env.TIME_ZONE);
