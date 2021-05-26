c2d60a986ba18ac6df451de2ec9a5dd07768bbf5
Todd <todd.rasband@podium.com>
2021-05-26 07:23:17 -0600

    change

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const {createNewDay, setSleepData, createNewWeek} = require('./notion');
 
 new CronJob('* * 1 * * *', createNewDay, null, true, process.env.TIME_ZONE);
 new CronJob('* * 12 * * *', setSleepData, null, true, process.env.TIME_ZONE);
-new CronJob('* * 1 * * Sunday', createNewWeek, null, true, process.env.TIME_ZONE);
+new CronJob('* * 1 * * 6', createNewWeek, null, true, process.env.TIME_ZONE);
