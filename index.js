5c5ea6fe221cf12ed740016071ec5df7c0f6d6a0
Todd <todd.rasband@podium.com>
2021-05-30 01:04:04 -0600

    decrease time of gets

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const CronJob = require('cron').CronJob;
 const { createNewDay, setSleepData, createNewWeek, setEatingData } = require('./notion');
 
 new CronJob('0 0 20 * * *', createNewDay, null, true, process.env.TIME_ZONE);
-new CronJob('0 0 */1 * * *', setSleepData, null, true, process.env.TIME_ZONE);
-new CronJob('0 0 */1 * * *', setEatingData, null, true, process.env.TIME_ZONE);
+new CronJob('0 */15 * * * *', setSleepData, null, true, process.env.TIME_ZONE);
+new CronJob('0 */15 * * * *', setEatingData, null, true, process.env.TIME_ZONE);
 new CronJob('0 0 1 * * 6', createNewWeek, null, true, process.env.TIME_ZONE);
