7e7cd5684b0af46af6c142a5ddc00773147a8310
Rasbandit <todd.rasband@gmail.com>
2021-10-27 22:34:55 -0600

    add tasker webhook

---
+++ endpoints/notionEndpoints.js
@@ -0,0 +1,21 @@
+const moment = require('moment');
+const { getDay } = require('../helpers/notionHelpers/notionDayAndWeekHelpers');
+const { updatePage } = require('../interfaces/notionInterface');
+
+const exportedValues = {};
+
+exportedValues.taskerWebhook = async (req, res) => {
+  const { date, secondsInCar } = req.body;
+  const formattedDate = moment(date, 'MM-DD-YY');
+
+  let minutesDrove = Math.round(secondsInCar/60)
+  const hoursDrove = Math.floor(minutesDrove/60)
+  minutesDrove -= hoursDrove * 60
+
+  const { id: pageId } = await getDay(formattedDate);
+  await updatePage(pageId, {properties: {"Drove Minutes": minutesDrove, "Drove Hours": hoursDrove}})
+
+  res.sendStatus(200);
+};
+
+module.exports = exportedValues;
--- index.js
+++ index.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 
 const express = require('express');
 const { startAllCrons } = require('./crons/cronTimers');
+const {taskerWebhook} = require('./endpoints/notionEndpoints')
 // require('./interfaces/googleCalendarInterface')
 
 const app = express();
 const { PORT } = process.env;
 
-app.post('/webhook/tasker', () => {})
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/webhook/tasker', taskerWebhook)
 
 app.listen(PORT, () => {
-  startAllCrons();
+  // startAllCrons();
   console.log(`Running full speed at ${PORT} MPH`)
 });
