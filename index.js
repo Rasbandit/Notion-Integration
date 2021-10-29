9d2fe71477ee3bd8989ccb616b115d26ad39689e
Rasbandit <todd.rasband@gmail.com>
2021-10-29 08:58:38 -0600

    started crons

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 
 const express = require('express');
 const { startAllCrons } = require('./crons/cronTimers');
-const {taskerWebhook} = require('./endpoints/notionEndpoints')
+const { taskerWebhook } = require('./endpoints/notionEndpoints');
 // require('./interfaces/googleCalendarInterface')
 
 const app = express();
 const { PORT } = process.env;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/webhook/tasker', taskerWebhook)
+app.post('/webhook/tasker', taskerWebhook);
 
 app.listen(PORT, () => {
-  // startAllCrons();
-  console.log(`Running full speed at ${PORT} MPH`)
+  startAllCrons();
+  console.log(`Running full speed at ${PORT} MPH`);
 });
